7d8b307e3f0eabcd01fad87f0ea17d27d1e19bcd
César Garza <c.garza.ibanez@gmail.com>
2023-06-26 22:20:06 -0600

    Update Header.js

--- task-app/frontend/src/components/Header.js
+++ task-app/frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <div className='header'>
-        <h1>Administrador de Tareas</h1>
+        <h1>Lista de Tareas</h1>
     </div>
   )
 }
